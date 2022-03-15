b147894f2ef48a122f034daf39d3d71077cd3847
Miles Thompson <utunga@gmail.com>
2022-03-16 00:31:37 +1300

    Add docs

---
+++ docs/precache-manifest.541a199391dfc1327217456067f9b812.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda76ee86b88e84c628bb82b48e92747",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a96f326ded9fd4f2861",
+    "url": "/static/css/2.07a18c37.chunk.css"
+  },
+  {
+    "revision": "0f30a194084cc3d8037d",
+    "url": "/static/css/main.dd456614.chunk.css"
+  },
+  {
+    "revision": "9a96f326ded9fd4f2861",
+    "url": "/static/js/2.31a53fbb.chunk.js"
+  },
+  {
+    "revision": "65fc15a8f4b1ad945ac580281e522806",
+    "url": "/static/js/2.31a53fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f30a194084cc3d8037d",
+    "url": "/static/js/main.c1fdf1ea.chunk.js"
+  },
+  {
+    "revision": "db7c1d7ae646ab50724c",
+    "url": "/static/js/runtime-main.48b5ae34.js"
+  },
+  {
+    "revision": "596e4d85fe9771aa206ae7d08da694f5",
+    "url": "/static/media/XCHC_Together_User_Personas.596e4d85.pdf"
+  },
+  {
+    "revision": "7db2ee086ac34f769f6f0a421c18102b",
+    "url": "/static/media/cashless_logo_white.7db2ee08.png"
+  },
+  {
+    "revision": "3e698adb36aff84c2f6118d2fe87ce47",
+    "url": "/static/media/clip-brainstorm.3e698adb.png"
+  },
+  {
+    "revision": "99ea816842a656fe997d9b6c77bb0d83",
+    "url": "/static/media/dmitriy.99ea8168.png"
+  },
+  {
+    "revision": "1b29a7d03bc514f8a98376fa45d288be",
+    "url": "/static/media/dmitriy_img_2.1b29a7d0.jpg"
+  },
+  {
+    "revision": "041de02896af136167e0d13d5e08bb47",
+    "url": "/static/media/figma_mocks.041de028.png"
+  },
+  {
+    "revision": "ca7b8bbd28b9f2eedfba3f297e43aa2b",
+    "url": "/static/media/kane.ca7b8bbd.png"
+  },
+  {
+    "revision": "639c186a570918756cc514a046e4d2b5",
+    "url": "/static/media/kane_img.639c186a.jpg"
+  },
+  {
+    "revision": "e651e11b1d432600ccd94b757e27f856",
+    "url": "/static/media/miles.e651e11b.png"
+  },
+  {
+    "revision": "b9da22bdf40cd42b9887812cbb4edff0",
+    "url": "/static/media/miles_img.b9da22bd.jpg"
+  },
+  {
+    "revision": "55f98161d68d2b88f51849fc8ea65ba8",
+    "url": "/static/media/mobile-frame.55f98161.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "2e7044fc0930da8358263942cfd2cde1",
+    "url": "/static/media/output.2e7044fc.ogv"
+  },
+  {
+    "revision": "5f17baaa36aa4b535e42bfed4235f94d",
+    "url": "/static/media/output.5f17baaa.webm"
+  },
+  {
+    "revision": "fbc3bf2e60766837e7e641ec63671f99",
+    "url": "/static/media/output.fbc3bf2e.mp4"
+  },
+  {
+    "revision": "200cee6c15b333947c71af1369bfabb6",
+    "url": "/static/media/preston.200cee6c.png"
+  },
+  {
+    "revision": "6a88650c309614ea41ba57c6c39f7bdb",
+    "url": "/static/media/preston_img.6a88650c.jpg"
+  },
+  {
+    "revision": "cec59c4f5c1c0bffa9d7d799c3e4ffe6",
+    "url": "/static/media/start_app.cec59c4f.png"
+  },
+  {
+    "revision": "6ebca47aaa50e78e095199df3cedea8f",
+    "url": "/static/media/together_logo_maybe.6ebca47a.png"
+  },
+  {
+    "revision": "57331861370622ff9cda37523114d13e",
+    "url": "/static/media/togetherproject_coop.57331861.png"
+  },
+  {
+    "revision": "65f3c46979901f0cdbc0a27736729f95",
+    "url": "/static/media/video_cover_min.65f3c469.png"
+  },
+  {
+    "revision": "431d9e9fce2f85d45c8abd5cd24dfea5",
+    "url": "/static/media/working.431d9e9f.jpg"
+  },
+  {
+    "revision": "f9142e6083ffc972dd0fab19eb486f9c",
+    "url": "/static/media/xchc_logo_white.f9142e60.png"
+  }
+]);
